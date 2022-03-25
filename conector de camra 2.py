f4ec256d4be718b05a9b4ad6dc7402dd97a01c4d
Danielamartine <kmartinezmo01@uninpahu.edu.co>
2022-03-24 19:07:18 -0500

    proyectodepruebas

---
+++ conector de camra 2.py
@@ -0,0 +1,21 @@
+from ast import Break
+import cv2
+import numpy as np
+
+
+
+#se crea el objeto del video 
+captura = cv2.VideoCapture(1)
+while True:
+    #se captura de camara a camara
+    (grabbed ,imagen ) = captura.read()
+    if not grabbed:
+        break
+
+    cv2.imshow("video Droidcam client",imagen)
+    tecla=cv2.waitKey(25) & 0xFF
+    if tecla==27:
+        break
+
+captura.relase()
+cv2.destroyAllWindows()
